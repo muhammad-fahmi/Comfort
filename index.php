5825693a9d305bb2c68049b5d532f95d9887fa3c
1808561084_muhammadfahmi <fahmipradana4@gmail.com>
2020-11-07 23:43:24 +0800

    update 7-11-2020

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
     <title>Landing Page</title>
 </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<body id="section1">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <a class="navbar-brand" href="http://localhost/Comfort/">
-            <img src="img/logo.png">
+            <img src="img/logo.png" width="150px" height="90px">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -25,32 +25,73 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#section1">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
+                        Products
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Man</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
+                        <a class="dropdown-item" href="#">Woman</a>
                     </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <button class="btn btn-secondary mr-sm-2">Register</button>
+                <button class="btn btn-secondary my-2 my-sm-0">Login</button>
             </form>
         </div>
     </nav>
+
+    <div id="carouselExampleCaptions" class="carousel slide car" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/cowo cewe.jpg" class="d-block w-100 tales" alt="Artboard 1">
+                <div class="carousel-caption d-none d-md-block" id="artboard1">
+                    <h3>Men and Women Apparel<br>Get it now and get 25% discount<br>
+                        <a href="#" class="btn-get-started animated fadeInUp scrollto">Purchase</a></h3>
+
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/cewe.jpg" class="d-block w-100 tales" alt="Artboard 2">
+                <div class="carousel-caption d-none d-md-block" style="text-align: right">
+                    <h1>November Deals<br>Women Top<br>Buy 2 get 1</h1>
+
+
+
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/cowo.jpg" class="d-block w-100 tales" alt="Artboard 3.png">
+                <div class="carousel-caption d-none d-md-block" style="color: white">
+
+                    <h2>Men Apparel November<br>Discount up to 60%</h2>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div class="contain">
+        <div class="main">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur dolorum quidem totam. Autem quisquam eligendi repellat obcaecati facilis commodi accusamus id? Distinctio eligendi expedita veniam sed fugit modi perferendis nemo, dolor explicabo amet fugiat officia iste blanditiis in cupiditate enim autem deserunt, aliquid dolorum quam repellat tenetur dolores. Doloribus amet ducimus ullam necessitatibus tenetur veniam quas obcaecati. Recusandae odit iste ut et saepe amet officiis similique, cumque mollitia exercitationem quas molestias ad quam accusamus, pariatur, laudantium rem. Atque sint porro dignissimos sapiente deleniti beatae. Enim, officia cupiditate, perferendis similique consectetur modi aperiam quas sed nisi consequuntur placeat veritatis obcaecati odio qui dolore! Expedita, recusandae. Nobis, reprehenderit. Quod hic eos eum mollitia error, quam voluptatum, iusto perferendis esse dicta amet consequatur cupiditate tempora laboriosam maxime facilis facere quas, deserunt deleniti maiores autem asperiores minus est sapiente. Cupiditate rem consequuntur ipsam expedita vel quae corporis iusto, et laboriosam maiores quisquam porro, sed, dicta fuga voluptatum? Quos neque libero id repudiandae itaque facilis alias, nulla eos, dicta repellendus aspernatur est architecto dolores sunt odio ad perspiciatis? Soluta impedit aliquid odit obcaecati natus consequuntur cumque. Pariatur laboriosam impedit quod rerum excepturi atque labore eveniet asperiores, nostrum iure soluta blanditiis! Laudantium odit ducimus harum incidunt?
+        </div>
+    </div>
+    <div class="clear"></div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
