ff891b433e094842f2da8edd7c33b373f8c41088
1808561080_Calvin Christian <68107761+calvin15k1@users.noreply.github.com>
2020-11-18 15:38:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <body id="section1">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <a class="navbar-brand" href="http://localhost/Comfort/">
-            Comfort
+            <img src="img/logo.png" width="150px" height="90px">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -39,6 +39,7 @@
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
+                <a href="" class="btn btn-secondary mr-sm-2">Register</a>
                 <a href="login/Login.html" class="btn btn-secondary my-2 my-sm-0">Login</a>
             </form>
         </div>
@@ -86,6 +87,36 @@
         </a>
     </div>
 
+    <section class="collection-section">
+        <div class="collection-container bd-grid">
+            <div class="collection-box">
+                <img src="img/cewe 1.jpg" alt="" class="colletion-img">
+
+                <div class="colletion-data">
+                    <h2 class="colletion-title"><span class="collection-subtitle">Women</span><br>Apparel</h2>
+                    <a href="#" class="collection-view">View Collection</a>
+                </div>
+            </div>
+
+            <div class="collection-box">
+                
+
+                <div class="colletion-data">
+                    <h2 class="colletion-title"><span class="collection-subtitle">Men</span><br>Apparel</h2>
+                    <a href="#" class="collection-view">View Collection</a>
+                
+                <img src="img/cowo 1.jpg" alt="" class="colletion-img"></div>
+            </div>
+        </div>
+    </div>
+
+    
+
+    <div class="contain">
+        <div class="main">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur dolorum quidem totam. Autem quisquam eligendi repellat obcaecati facilis commodi accusamus id? Distinctio eligendi expedita veniam sed fugit modi perferendis nemo, dolor explicabo amet fugiat officia iste blanditiis in cupiditate enim autem deserunt, aliquid dolorum quam repellat tenetur dolores. Doloribus amet ducimus ullam necessitatibus tenetur veniam quas obcaecati. Recusandae odit iste ut et saepe amet officiis similique, cumque mollitia exercitationem quas molestias ad quam accusamus, pariatur, laudantium rem. Atque sint porro dignissimos sapiente deleniti beatae. Enim, officia cupiditate, perferendis similique consectetur modi aperiam quas sed nisi consequuntur placeat veritatis obcaecati odio qui dolore! Expedita, recusandae. Nobis, reprehenderit. Quod hic eos eum mollitia error, quam voluptatum, iusto perferendis esse dicta amet consequatur cupiditate tempora laboriosam maxime facilis facere quas, deserunt deleniti maiores autem asperiores minus est sapiente. Cupiditate rem consequuntur ipsam expedita vel quae corporis iusto, et laboriosam maiores quisquam porro, sed, dicta fuga voluptatum? Quos neque libero id repudiandae itaque facilis alias, nulla eos, dicta repellendus aspernatur est architecto dolores sunt odio ad perspiciatis? Soluta impedit aliquid odit obcaecati natus consequuntur cumque. Pariatur laboriosam impedit quod rerum excepturi atque labore eveniet asperiores, nostrum iure soluta blanditiis! Laudantium odit ducimus harum incidunt?
+        </div>
+    </div>
     <div class="clear"></div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
