c8f449064e02e57e9b801fc8c635d0453e599e32
muh_fahmi <fahmipradana4@gmail.com>
2020-11-29 12:55:21 +0800

    update all

--- index.php
+++ index.php
@@ -7,92 +7,102 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Landing Page</title>
+    <title>Comfort</title>
 </head>
 
 <body>
-    <!-----
-        <div class="preloader" id="preloader"></div>
-    -->
-    
-
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <a class="navbar-brand" href="#">Comfort</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#"><img src="./img/comfort-logo.png" width="100px"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active px-2">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">About Us</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownId">
-                        <a class="dropdown-item" href="#">Action 1</a>
-                        <a class="dropdown-item" href="#">Action 2</a>
-                    </div>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Contact</a>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <form class="form-inline my-2 my-lg-0" method="post" action="login/index.html">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Login</button>
             </form>
         </div>
     </nav>
-    
-    <section class="home">
-        <img src="img/3.png" alt="img/2.png" id="bg">
-        <img src="img/1.png" alt="img/1.png" id="org">
-        <h2 id="text">Anywhere You Go <br>We Comfort You</h2>
-    </section>
 
-    <div class="container">
-        <div class="container-box bd-grid" id="Container">
-            <div class="container-bg">
-                <img src="img/cewe 1.jpg" alt="" class="img-container">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./img/gambar-carousel-1.jpg" class="d-block w-100" alt="gambar-carousel-1">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-1">Men Apparel November<br>Discount up to 60%<br><br><a href="#"
+                            class="btn btn-outline-light">Purchase</a></h5>
+                </div>
             </div>
-            <h2 class="title">Women Apparel<br><a href="#" class="subtitle">View Collection</a></h2>
-            
-        </div>
-
-        <div class="container-box">
-            <div class="container-bg">
-                <img src="img/cowo 1.jpg" alt="" class="img-container">
+            <div class="carousel-item">
+                <img src="./img/gambar-carousel-2.jpg" class="d-block w-100" alt="gambar-carousel-2">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-2">Men and Women Apparel<br>Get it now and get 25% discount<br><br><a href="#"
+                            class="btn btn-outline-dark">Purchase</a></h5>
+                </div>
             </div>
-            <h2 class="title">Men Apparel<br><a href="#" class="subtitle">View Collection</a></h2>
-            
+            <div class="carousel-item">
+                <img src="./img/gambar-carousel-3.jpg" class="d-block w-100" alt="gambar-carousel-3">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-3">November Deals<br>Women Top<br>Buy 2 get 1<br><br><a href="#"
+                            class="btn btn-outline-dark">Purchase</a></h5>
+                </div>
+            </div>
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 
-    
-
-    <div class="contain">
+    <div class="container col-12 mt-5 d-flex justify-content-center">
         <div class="about">
             <h4>About Us</h4>
-			<p>
-				Comfort is a brand from 3 man from Indonesia who want a nice, trendy, and cheap apparel but with High Quality Fabric for <br>Comfort come with Men and Women Apparel thats other mainstream online clothing shop doesn't have<br>We believe that people who wear comfort apparel they will got comfort and feel happy everyday</p>
+            <p>
+                Comfort is a brand from 3 man from Indonesia who want a nice, trendy, and cheap apparel but with High
+                Quality Fabric for <br>Comfort come with Men and Women Apparel thats other mainstream online clothing
+                shop doesn't have<br>We believe that people who wear comfort apparel they will got comfort and feel
+                happy everyday</p>
         </div>
     </div>
     <div class="clear"></div>
 
     <!-- Footer -->
-<footer class="page-footer">
-    <div class="container">
-      <!-- Social Buttons -->
+    <footer class="page-footer">
+        <div class="container">
+            <!-- Social Buttons -->
 
-      <!-- Social buttons -->
-  
-    </div>
-    <div class="footer-copyright text-center py-3">©Comfort 2020 Copyright</div>
-  </footer>
+            <!-- Social buttons -->
+
+        </div>
+        <div class="footer-copyright text-center py-3">&copy; Comfort 2020 Copyright</div>
+    </footer>
 
 
     <script src="jquery/jquery-3.5.1.min.js"></script>
--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,47 @@
 <?php
+class database
+{
+	var $host = "localhost";
+	var $username = "root";
+	var $password = "";
+	var $database = "comfort";
+	var $koneksi;
 
-$koneksi = mysqli_connect("localhost", "root", "", "comfort");
+	function __construct()
+	{
+		$this->koneksi = mysqli_connect($this->host, $this->username, $this->password, $this->database);
+	}
 
-// Check connection
-if (mysqli_connect_errno()) {
-	echo "Koneksi database gagal : " . mysqli_connect_error();
+
+	function register($username, $password, $nama)
+	{
+		$insert = mysqli_query($this->koneksi, "insert into tb_user values ('','$username','$password','$nama')");
+		return $insert;
+	}
+
+	function login($username, $password, $remember)
+	{
+		$query = mysqli_query($this->koneksi, "select * from tb_user where username='$username'");
+		$data_user = $query->fetch_array();
+		if (password_verify($password, $data_user['password'])) {
+
+			if ($remember) {
+				setcookie('username', $username, time() + (60 * 60 * 24 * 5), '/');
+				setcookie('nama', $data_user['nama'], time() + (60 * 60 * 24 * 5), '/');
+			}
+			$_SESSION['username'] = $username;
+			$_SESSION['nama'] = $data_user['nama'];
+			$_SESSION['is_login'] = TRUE;
+			return TRUE;
+		}
+	}
+
+	function relogin($username)
+	{
+		$query = mysqli_query($this->koneksi, "select * from tb_user where username='$username'");
+		$data_user = $query->fetch_array();
+		$_SESSION['username'] = $username;
+		$_SESSION['nama'] = $data_user['nama'];
+		$_SESSION['is_login'] = TRUE;
+	}
 }
