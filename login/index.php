cbbde13fa5d2f2a7f9ae0f3688fa699035d6cc2d
muh_fahmi <fahmipradana4@gmail.com>
2020-12-12 21:10:50 +0800

    update 12-12-2020

--- login/index.php
+++ login/index.php
@@ -1,24 +1,10 @@
-<?php
-include_once('../koneksi.php');
-$database = new database();
-if (isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $nama = $_POST['nama'];
-    if ($database->register($username, $password, $nama)) {
-        header('location:login.php');
-    }
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Raleway"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
@@ -30,29 +16,26 @@ if (isset($_POST['submit'])) {
                 <h2>LOGIN</h2>
                 <div class="underline-title"></div>
             </div>
-            <form method="POST" class="form" name="formInput"
-                onsubmit="validasiEmail();">
-                <label for="user-email"
-                    style="padding-top:13px">&nbsp;Email</label>
-                <input id="user-email" class="form-content" type="text"
-                    name="email" autocomplete="off" required />
+            <form method="POST" class="form" name="formInput" onsubmit="validasiEmail();">
+                <label for="user-username" style="padding-top:13px">&nbsp;Username</label>
+                <input id="user-username" class="form-content" type="text" name="username" autocomplete="off" required />
                 <div class="form-border"></div>
-                <label for="user-password"
-                    style="padding-top:22px">&nbsp;Password</label>
-                <input id="user-password" class="form-content" type="password"
-                    name="password" required />
+                <label for="user-password" style="padding-top:22px">&nbsp;Password</label>
+                <input id="user-password" class="form-content" type="password" name="password" required />
                 <div class="form-border"></div>
-                <a href="#">
-                    <legend id="forgot-pass">Forgot password?</legend>
-                </a>
-                <input id="submit-btn" type="submit" name="submit"
-                    value="LOGIN" />
-                <a href="../register/index.php" id="signup">Don't have
-                    account yet?</a>
+                <br>
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me" name="remember">Remember me
+                    </label>
+                </div>
+
+                <input id="submit-btn" type="submit" name="submit" value="LOGIN" />
+                <a href="../register/index.php" id="signup">Don't have account yet?</a>
             </form>
         </div>
     </div>
-    <script src="./script.js"></script>
+    <script src="script.js"></script>
 </body>
 
 </html>
